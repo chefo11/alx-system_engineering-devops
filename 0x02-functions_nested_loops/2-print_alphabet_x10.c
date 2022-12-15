d633014a324e49b6eedac08bf3413ce95bd0c1fd
Chefo11 <fran6cool@gmail.com>
2022-12-14 20:50:51 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10(void) - prints abs x10
+ * Return: always 0
+ */
+void print_alphabet_x10(void)
+{
+	char j, i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(x);
+		}
+		_putchar('\n');
+	}
+}
