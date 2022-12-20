941a9bf7aeea50ffc6d0021f0e8688b34f8ac8c7
Chefo11 <fran6cool@gmail.com>
2022-12-19 19:02:51 -0800

    1-swap.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
+
 /*
+ * Description: prints the numbers 1 - 100
  * main - prints Buzz each numbers of 3 and 5.
- * Return: Always 0.
+ * Return: Always 0
  */
 
 int main(void)
 {
-	int n;
+	int i;
 
-	n = 1;
 	printf("%d", n);
-	for (n = 2; n <= 100; n++)
+	for (i = 1; i <= 100; i++)
 	{
-		if ((n % 3 == 0) && (n % 5 == 0))
+		if (i % 15 == 0)
 		{
 			printf(" FizzBuzz");
 		}
-		else if (n % 3 == 0)
+		else if (i % 3 == 0)
 		{
 			printf(" Fizz");
 		}
-		else if (n % 5 == 0)
+		else if (i % 5 == 0)
 		{
 			printf(" Buzz");
 		}
@@ -28,6 +29,8 @@ int main(void)
 		{
 			printf(" %d", n);
 		}
+		if (i < 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
